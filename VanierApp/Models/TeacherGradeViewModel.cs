85f0691d3bf3f78980adfb43835fd441cc3d4a1b
Jean Carlos Guzman <jeancarlosg93@gmail.com>
2024-10-08 15:16:39 -0400

    Modified Connection string and created TeacherGradeViewModel.cs

---
+++ VanierApp/Models/TeacherGradeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VanierApp.Models;
+
+public class TeacherGradeViewModel
+{
+   String GradeCode{get;set;}
+   String GradeCOmment{get;set;}
+   String StudentID{get;set;}
+   String StudentaName{get;set;}
+}
