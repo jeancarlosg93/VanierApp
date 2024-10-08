bb7a986933458b4004bcd398363671a60cb25643
Jean Carlos Guzman <jeancarlosg93@gmail.com>
2024-10-08 15:30:55 -0400

    Revert "Modified Connection string and created TeacherGradeViewModel.cs"
    This reverts commit 85f0691d3bf3f78980adfb43835fd441cc3d4a1b.

--- VanierApp/Models/TeacherGradeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VanierApp.Models;
-
-public class TeacherGradeViewModel
-{
-   String GradeCode{get;set;}
-   String GradeCOmment{get;set;}
-   String StudentID{get;set;}
-   String StudentaName{get;set;}
-}
